700f80fe8db8b5d15cf160aa7a5434f1bf006053
Stanley Winata <winatastanley355@gmail.com>
2025-08-09 15:14:41 +0700

    Phone

--- src/middleware/request/phone.rs
+++ src/middleware/request/phone.rs
@@ -1,4 +1,4 @@
 use regex::Regex;
 use std::sync::LazyLock;
 
-pub static RE_PHONE: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"^8\d*$").unwrap());
+pub static RE_PHONE: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"^08\d*$").unwrap());
